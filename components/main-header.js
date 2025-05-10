746a8daf052c45e25ba3a3b9899b86773937004e
Andrea Huang <huangandrea@hotmail.it>
2025-05-10 17:47:45 +0200

    Using More Custom Components (MainHeaderBackground)

--- components/main-header.js
+++ components/main-header.js
@@ -3,32 +3,36 @@ import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href='/'>
-        {/* 
-        optimized image: 
-        - lazy loading (loading="lazy")
-        - automatic image size detection (width="1024" height="1024")
-        - different image size depending on device and viewport (srcset="")
-        - priority: if image MUST be rendered
-         */}
-        <Image src={logoImg} alt='A plate with food on it' priority/>
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          {/* 
+      optimized image: 
+      - lazy loading (loading="lazy")
+      - automatic image size detection (width="1024" height="1024")
+      - different image size depending on device and viewport (srcset="")
+      - priority: if image MUST be rendered
+       */}
+          <Image src={logoImg} alt='A plate with food on it' priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href='/meals'>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href='/community'>Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
