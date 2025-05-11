7afe226341eace6252a3c91b3ff0093d5e23b977
Andrea Huang <huangandrea@hotmail.it>
2025-05-11 16:12:25 +0200

    Using Suspense & Streamed Responses For Granular Loading State Management (show data-independent parts sooner)

--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,5 +1,7 @@
 import classes from "./loading.module.css"
 
+// loading.js is a reserved file name for loading pages
+// now we want to use another approach for better UX
 export default function MealsLoadingPage() {
     return <p className={classes.loading}>Fetching meals ...</p>
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -3,24 +3,33 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, create {" "}
+          Delicious meals, create{" "}
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>Choose your favorie recipe and cook it yourself. It easy and fun.</p>
         <p className={classes.cta}>
-          <Link href="/meals/share">Share your favorite recipe</Link>
+          <Link href='/meals/share'>Share your favorite recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals ...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
