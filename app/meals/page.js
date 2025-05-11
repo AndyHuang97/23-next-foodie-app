80d21647dfe6fa830d36abc73d840fdd6d4d2a2f
Andrea Huang <huangandrea@hotmail.it>
2025-05-11 15:50:32 +0200

    Outputting Meals Data & Images With Unknown Dimensions (use "fill")

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,24 @@
 import Link from "next/link";
 
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>The Meals</h1>
-      <p>
-        <Link href='/meals/meal-1'>Meal 1</Link>
-      </p>
-      <p>
-        <Link href='/meals/meal-2'>Meal 2</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, create {" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorie recipe and cook it yourself. It easy and fun.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   );
 }
---
+++ components/meals/meal-item.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import classes from './meal-item.module.css';
+
+export default function MealItem({ title, slug, image, summary, creator }) {
+  return (
+    <article className={classes.meal}>
+      <header>
+        <div className={classes.image}>
+          {/* NOTE: this image is not imported manually from public folder,
+          but from the database. 
+          In this case, nextjs is not able to detect the dimensions of image 
+          beforehand at load time but only at runtime, so use "fill"
+           */}
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h2>{title}</h2>
+          <p>by {creator}</p>
+        </div>
+      </header>
+      <div className={classes.content}>
+        <p className={classes.summary}>{summary}</p>
+        <div className={classes.actions}>
+          <Link href={`/meals/${slug}`}>View Details</Link>
+        </div>
+      </div>
+    </article>
+  );
+}
