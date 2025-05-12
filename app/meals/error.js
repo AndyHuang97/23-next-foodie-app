971eec2226c2a48e93cf773f5f97461b004ee9c5
Andrea Huang <huangandrea@hotmail.it>
2025-05-12 13:11:16 +0200

    Creating a Slug & Sanitizing User Input for XSS Protection

--- app/meals/error.js
+++ app/meals/error.js
@@ -3,7 +3,7 @@
 export default function Error() {
   return (
     <main className='error'>
-      <h1>An error occurre!</h1>
+      <h1>An error occurred!</h1>
       <p>Failed to fetch meal data.</p>
     </main>
   );
